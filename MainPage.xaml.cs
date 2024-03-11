27952dbb8b17a64a0551adeac498a53ca572927b
massimo trovati <meshmi@outlook.it>
2024-03-11 07:28:54 +0100

    QR UWP
    QR UWP APP

---
+++ MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+using ZXing;
+using ZXing.Common;
+
+// Il modello di elemento Pagina vuota è documentato all'indirizzo https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x410
+
+namespace QR64v1
+{
+    /// <summary>
+    /// Pagina vuota che può essere usata autonomamente oppure per l'esplorazione all'interno di un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private object txtQRCodeText;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        private void GenerateQRCode(object sender, RoutedEventArgs e)
+        {
+            string text = txtInput.Text;
+            if (!string.IsNullOrEmpty(text))
+            {
+                BarcodeWriter writer = new BarcodeWriter
+                {
+                    Format = BarcodeFormat.QR_CODE,
+                    Options = new EncodingOptions
+                    {
+                        Height = 300,
+                        Width = 300
+                    }
+                };
+
+                WriteableBitmap bitmap = writer.Write(text);
+                imgQRCode.Source = bitmap;
+            }
+            else
+            {
+                // Show an error message
+                var dialog = new ContentDialog
+                {
+                    Title = "Error",
+                    Content = "Please enter text to generate QR code",
+                    CloseButtonText = "Ok"
+                };
+                dialog.ShowAsync();
+            }
+        }
+    }
+}
